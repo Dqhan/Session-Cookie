b6b21aa8f4fcd534b1244de59bb81e8324350cda
dqhan <291927006@qq.com>
2019-04-30 01:01:14 +0800

    demo

--- app.js
+++ app.js
@@ -12,6 +12,57 @@ var app = express();
 app.use('/', express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('trust proxy', 1);
+// app.use(cookie('express_cookie'));
+//cookie
+// app.post('/login', function (req, res) {
+//     User.findOne({
+//         username: req.body.username
+//     }).then(function (userInfo) {
+//         if (!userInfo) {
+//             console.log('user is not exist.');
+//             return;
+//         }
+//         var data = {};
+//         data['username'] = userInfo.username;
+//         data['password'] = userInfo.password;
+//         res.cookie('username', JSON.stringify(data), { maxAge: 900000, httpOnly: true });
+//         res.status(200).json(data);
+//     })
+//         .catch(function (e) {
+//             console.log(e);
+//         })
+// })
+
+// session 
+// app.use(cookie('express_cookie'));
+// app.use(session({
+//     secret: 'express_cookie',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { maxAge: 60 * 1000 * 30 },
+//     rolling: true,
+// }));
+
+// app.post('/login', function (req, res) {
+//     User.findOne({
+//         username: req.body.username
+//     }).then(function (userInfo) {
+//         if (!userInfo) {
+//             console.log('user is not exist.');
+//             return;
+//         }
+//         var data = {};
+//         data['username'] = userInfo.username;
+//         data['password'] = userInfo.password;
+//         req.session.userInfo = data;
+//         res.status(200).json(data);
+//     })
+//         .catch(function (e) {
+//             console.log(e);
+//         })
+// })
+
+//session in mongodb
 app.use(cookie('express_cookie'));
 app.use(session({
     secret: 'express_cookie',
@@ -20,30 +71,51 @@ app.use(session({
     cookie: { maxAge: 60 * 1000 * 30 },
     rolling: true,
     store: new MongoStore({
-        url: 'mongodb://localhost:27107/test',
+        url: 'mongodb://127.0.0.1:27017/demo',
         collection: 'sessions'
     })
 }));
-
 app.post('/login', function (req, res) {
     User.findOne({
         username: req.body.username
-    }).then(function (res) {
-        if (!res) {
+    }).then(function (userInfo) {
+        if (!userInfo) {
             console.log('user is not exist.');
             return;
         }
         var data = {};
-        data['username'] = res.username;
-        data['userId'] = res._id;
+        data['username'] = userInfo.username;
+        data['password'] = userInfo.password;
         req.session.userInfo = data;
+        res.status(200).json(data);
     })
         .catch(function (e) {
             console.log(e);
         })
 })
 
+//session in redis
+
+app.use(session({
+    name : "sid",
+    secret : 'Asecret123-',
+    resave : true,
+    rolling:true,
+    saveUninitialized : false,
+    cookie : config.cookie,
+    store : new RedisStrore(config.sessionStore)
+}));
+
 app.get('/userInfo', function (req, res) {
+    //cookie
+    // if (req.cookies.userInfo) {
+    //     console.log('login successfully')
+    // }
+    // else {
+    //     console.log('session timeout.');
+    // }
+    // res.status(200).json(req.cookies.userInfo);
+    // session
     if (req.session.userInfo) console.log('login successfully');
     else console.log('session timeout.');
 })
@@ -81,7 +153,7 @@ app.post('/register', function (req, res) {
         })
 })
 
-mongoose.connect('mongodb://127.0.0.1:27017/test', function (err) {
+mongoose.connect('mongodb://127.0.0.1:27017/demo', function (err) {
     if (err) {
         console.log(err, 'error');
         return;
